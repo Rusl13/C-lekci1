f7b031fd2a7c947b46d33d6d82f9cea8ffe7bc50
Rusl <hovarus@bk.ru>
2022-06-09 13:20:46 +0300

    Опертор условия

---
+++ uslovie(if_else)/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if 
+(username.ToLower() == "маша")
+{
+   Console.WriteLine("Авэ, Маша");
+ 
+}
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+}
